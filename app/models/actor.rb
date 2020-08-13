469f7abb903534cb32fad8c7c1f744fb53195ac5
Nunnyr <nunnyreyes@gmail.com>
2020-08-13 17:22:29 -0400

    Passed belongs to/ has to

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,7 +1,12 @@
 class Actor < ActiveRecord::Base
-  has_many: shows
-  has_many: characters
+  has_many :shows
+  has_many :characters
 
+  #now I want to create the full_name
+  def
+    binding.pry
+
+  end
 
 
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,5 +1,5 @@
 class Show < ActiveRecord::Base
-  has_many: characters
-  has_many: actors
-  belongs_to: show
+  has_many :characters
+  has_many :actors
+  belongs_to :show
 end
